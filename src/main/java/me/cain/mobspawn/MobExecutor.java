9735400f7338192532ceae7351ac69c51f8a85fb
Cain <cain.donaghey@live.co.uk>
2011-11-30 08:11:56 +0000

    Minor Cleanup

--- src/main/java/me/cain/mobspawn/MobExecutor.java
+++ src/main/java/me/cain/mobspawn/MobExecutor.java
@@ -12,18 +12,19 @@ public class MobExecutor implements CommandExecutor
 	public boolean onCommand(CommandSender s, Command c, String l, String [] args)
 	{
 		if(s instanceof Player) {
+			Player sender = (Player) s;
 			if(c.getName().equalsIgnoreCase("mob")) {
 				if(args.length < 1) { return false; }
 				else {
-					if(s.hasPermission("mob.spawn")) {
+					if(MobSpawn.hasNode(sender, "mob.spawn")) {
 						try {
-							((Player) s).getWorld().spawnCreature(((Player) s).getTargetBlock(null, 0).getLocation(), CreatureType.valueOf(args[0].toUpperCase()));
+							sender.getWorld().spawnCreature(sender.getTargetBlock(null, 0).getLocation(), CreatureType.valueOf(args[0].toUpperCase()));
 						} catch (Exception e) {
-							Util.deny((Player) s, "Something went wrong. Try again?");
+							Util.deny(sender, "Something went wrong. Try again?");
 						}
-						Util.success((Player) s, "Mob spawned successfully!");
+						Util.success(sender, "Mob spawned successfully!");
 					} else {
-						Util.deny((Player) s, "Mob spawn denied. No permission.");
+						Util.deny(sender, "Mob spawn denied. No permission.");
 					}
 				}
 			}
--- src/main/java/me/cain/mobspawn/MobSpawn.java
+++ src/main/java/me/cain/mobspawn/MobSpawn.java
@@ -1,18 +1,49 @@
 package main.java.me.cain.mobspawn;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class MobSpawn extends JavaPlugin
 {
+	public static PermissionHandler permHandler;
+	public static MobSpawn plugin;
+	
 	public void onEnable()
 	{
 		getServer().getPluginCommand("mob").setExecutor(new MobExecutor());
 		Util.console("[" + this.getDescription().getName() + "] MobSpawner has been enabled!");
+		plugin = this;
 	}
 	
 	public void onDisable()
 	{
 		Util.console("[" + this.getDescription().getName() + "] MobSpawner has been disabled!");
+	}
+	
+	public void OldPermissions()
+	{
+		Plugin Perms = getServer().getPluginManager().getPlugin("Permissions");
+		if(Perms == null) {
+			return;
+		}
+		else
+		{
+		    permHandler = ((Permissions) Perms).getHandler();
+		    Util.console("[" + this.getDescription().getName() + "] Permissions detected!");
+		}
+	}
+	
+	public static boolean hasNode(Player p, String node) {
+		if(Bukkit.getServer().getPluginManager().getPlugin("Permissions") != null) {
+			return permHandler.has(p, node);
+		} else {
+			return p.hasPermission(node);
+		}
 	}
 	
 }
